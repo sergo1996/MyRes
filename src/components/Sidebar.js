1190dacf97f58bf5f1a34fa41b46cda30a125676
sergo1996 <56196384+sergo1996@users.noreply.github.com>
2020-11-29 20:33:53 +0200

    add Heigh

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import SidebarNav from "./SidebarNav";
 import AboutMe from "./aboutMe/AboutMe";
 import MySkills from "./mySkills/MySkills";
-
 const Sidebar = () => {
   const [isAboutMe, setIsAboutMe] = useState(true);
 
